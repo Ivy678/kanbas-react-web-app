992f1b710bfa53f55ebef71ace2452750334b97a
ZichuanZhu <zzc1922094069@gmail.com>
2023-11-20 22:43:51 -0500

    a5 REST fa23

--- src/Kanbas/Courses/Modules/client.js
+++ src/Kanbas/Courses/Modules/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-const COURSES_URL = "http://localhost:4000/api/courses";
-const MODULES_URL = "http://localhost:4000/api/modules";
+const COURSES_URL = "https://kanbas-node-server-app-rcwd.onrender.com/api/courses";
+const MODULES_URL = "https://kanbas-node-server-app-rcwd.onrender.com/api/courses";
 
 export const updateModule = async (module) => {
     const response = await axios.
